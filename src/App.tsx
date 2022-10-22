c29c47f012fdc4e7e7d88ecc6c46f53186c22f6c
AlessandraMayumi <alessandra_mms@hotmail.com>
2022-10-22 12:16:58 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './styles/global.css';
+
+export function App() {
+  return (
+    <>
+      <h1>Hello World</h1>
+      <button className="bg-cyan-300">
+        Enviar
+      </button>
+    </>
+  )
+}
---
+++ src/components/Checkbox.stories.tsx
@@ -0,0 +1,11 @@
+import { Meta, StoryObj } from '@storybook/react'
+import { Checkbox, CheckboxProps } from './Checkbox'
+
+export default {
+    title: 'Components/Checkbox',
+    component: Checkbox,
+    args: {},
+    argTypes: {}
+} as Meta
+
+export const Default: StoryObj<CheckboxProps> = {}
